1be0ad501ca712834109d0465e6c36ecf4aa459e
Nick Kniola <nickkniola@me.com>
2020-11-10 14:42:51 -0800

    Created functions to retrieve price, past price, symbol data from CoinGecko API #1

--- js/main.js
+++ js/main.js
@@ -18,3 +18,29 @@ function toggleNav() {
     navRows[1].className = 'nav-row item hidden';
   }
 }
+
+function getPrice(cryptocurrency) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=' + cryptocurrency + '&vs_currencies=usd');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log('current price:', xhr.response.litecoin.usd);
+  });
+  xhr.send();
+}
+
+getPrice('litecoin');
+
+function getPastPrice(cryptocurrency, date) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.coingecko.com/api/v3/coins/' + cryptocurrency + '/history?date=' + date);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log('xhr.response:', xhr.response);
+    console.log('past price for ' + date, xhr.response.market_data.current_price.usd);
+    console.log('symbol: ', xhr.response.symbol);
+  });
+  xhr.send();
+}
+
+getPastPrice('litecoin', '01-11-2020');
