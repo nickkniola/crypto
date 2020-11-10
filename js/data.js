ab727ced26b35246b54728496818b9139755398b
Nick Kniola <nickkniola@me.com>
2020-11-10 13:21:49 -0800

    Created data model

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 /* exported data */
+var data = [];
+var crypto = {
+  name: '',
+  symbol: '',
+  price: '',
+  pastPrices: {
+    oneMonth: '',
+    threeMonths: '',
+    sixMonths: '',
+    oneYear: '',
+    fiveYears: ''
+  }
+};
